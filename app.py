8296aef33a218b6c51c0f2ecfba389fea654676a
bhuneshwer <bhuneshwer.jaiswal@litmusworld.com>
2020-04-19 16:39:01 +0530

    Added new route

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template,redirect,url_for
 from flask import request
 from markupsafe import escape
+from newspaper import fulltext
 from newspaper import Article
 import requests
 import nltk
@@ -14,39 +15,32 @@ except AttributeError:
     pass
 else:
     ssl._create_default_https_context = _create_unverified_https_context
-   
+
 app = Flask(__name__)
+
+
+
 
 @app.route('/')
 def index():
-    return 'Index Page'
+    return 'Welcome to news-analyzer api.'
 
-@app.route('/hello')
-def hello():
-    return 'Hello, World'
 
-@app.route('/user/<username>')
+@app.route('/hello/<username>')
 def show_user_profile(username):
     # show the user profile for that user
     return 'User %s' % escape(username)
 
-@app.route('/post/<int:post_id>')
-def show_post(post_id):
-    # show the post with the given id, the id is an integer
-    return 'Post %d' % post_id
 
-@app.route('/dashboard/<name>')
-def dashboard(name):
-   return 'welcome %s' % name
-
-@app.route('/login',methods = ['POST', 'GET'])
-def login():
-   if request.method == 'POST':
-      user = request.form['name']
-      return redirect(url_for('dashboard',name = user))
-   else:
-      user = request.args.get('name')
-      return render_template('login.html')
+@app.route("/get_html",methods=['GET'])
+def get_html():
+    news_url = request.args.get("url")
+    try:
+      if(news_url and len(news_url)):
+        html = requests.get(news_url).text
+        return render_template('news-summary.html', news_html=fulltext(html))
+    except Exception as e:
+      return render_template('news-summary.html', news_html=e)
 
 
 @app.route('/analyze',methods=['GET'])
@@ -58,6 +52,8 @@ def analyze():
         article.parse()
         nltk.download("punkt")
         article.nlp()
+
+
         content = {
             "authors"  : article.authors,
             "publish_date" : article.publish_date,
@@ -65,12 +61,10 @@ def analyze():
             "article_text" : article.text,
             "article_title" : article.title,
             "article_summary" : article.summary,
-            #"keywords":article.keywords,
-            #"tags":article.tags
+            "keywords":article.keywords
         }
         return content
 
-    #return render_template('news-summary.html',**content)
  
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
